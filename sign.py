d463b6fe79c91c2d62e4d71c8a7bbe60b951f450
leaf-lxh <meleaf@qq.com>
2018-02-13 19:36:35 +0800

    added function GetTBS()

--- sign.py
+++ sign.py
@@ -1,4 +1,5 @@
 import urllib.request
+
 def GetFID(kw):
 
 	url="http://tieba.baidu.com/f/commit/share/fnameShareApi?ie=utf-8&fname=" + str(kw)
@@ -10,5 +11,22 @@ def GetFID(kw):
 
 	return str(Response)[PositionA:PositionB]
 
+def GetTBS(kw,BDUSS):
+
+	url="http://tieba.baidu.com/mo/m?kw=" + str(kw)
+	Headers = {	
+			"User-Agent" : "bdtb for Android 9.0",
+			"Cookie" : "BDUSS=" + BDUSS
+		  }
+	thisRequest = urllib.request.Request(url,headers=Headers)
+	thisRequest = urllib.request.urlopen(thisRequest)
+	response = str(thisRequest.read())
+	PositionA = response.find(r'name="tbs" value="')
+	PositionA += len(r'name="tbs" value="')
+	PositionB = response.find('\"' , PositionA)
+	
+	return response[PositionA:PositionB]
+
 def SignIn(kw, BDUSS):
-	fid = GetFID(kw)	
+	fid = GetFID(kw)
+		

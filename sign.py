cdd8a41e1be9aa96928c93f9bdb69716a0167c6f
leaf-lxh <meleaf@qq.com>
2018-02-13 20:51:44 +0800

    added sign in function

--- sign.py
+++ sign.py
@@ -1,4 +1,5 @@
 import urllib.request
+import hashlib
 
 def GetFID(kw):
 
@@ -11,11 +12,11 @@ def GetFID(kw):
 
 	return str(Response)[PositionA:PositionB]
 
-def GetTBS(kw,BDUSS):
+def GetTBS(kw , BDUSS):
 
 	url="http://tieba.baidu.com/mo/m?kw=" + str(kw)
 	Headers = {	
-			"User-Agent" : "bdtb for Android 9.0",
+			"User-Agent" : "bdtb for Android 8.0",
 			"Cookie" : "BDUSS=" + BDUSS
 		  }
 	thisRequest = urllib.request.Request(url,headers=Headers)
@@ -24,9 +25,22 @@ def GetTBS(kw,BDUSS):
 	PositionA = response.find(r'name="tbs" value="')
 	PositionA += len(r'name="tbs" value="')
 	PositionB = response.find('\"' , PositionA)
-	
 	return response[PositionA:PositionB]
 
-def SignIn(kw, BDUSS):
+def SignIn(kw , BDUSS):
+
+	url = "http://c.tieba.baidu.com/c/c/forum/sign"
+
 	fid = GetFID(kw)
-		
+	tbs = GetTBS(kw , BDUSS)
+	parameter="BDUSS=" + BDUSS + "&fid=" + fid + "&kw=" + kw + "&tbs=" +tbs + "&sign="
+	sign = "BDUSS=" + BDUSS + "fid=" + fid + "kw=" + kw + "tbs=" +tbs + "tiebaclient!!!"
+	
+	signMD5 = hashlib.md5()
+	signMD5.update(sign.encode("utf8"))
+	sign = str(signMD5.hexdigest()).upper()
+	parameter += sign
+	
+	thisRequest = urllib.request.urlopen(url,data=bytes(parameter,"utf8"))	
+	response = str(thisRequest.read())
+	return response

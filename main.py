9f070da29b61f3662c9701594ce9e066732b1f3e
leaf-lxh <meleaf@qq.com>
2018-02-20 00:46:04 +0800

    added autumatic sign in set up logic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#main.py
 import task
 import sign
 import json
@@ -23,12 +24,14 @@ if configData == "" :
 else :
 	myInfo = json.loads(configData)
 		
+Task =task.SignInTask(myInfo["BDUSS"],myInfo["STOKEN"]) #Initialize SignInTask instance
 
 while True:
 	print (
 """[1]Sign in one bar
-[2]Sign all
-[3]Set up automatic signing"""
+[2]Sign in all
+[3]Set up automatic sign in
+[4]Disable automatic sign in"""
 		)
 	thisResult = int(input("What you want today:"))
 	if thisResult == 1:
@@ -36,4 +39,20 @@ while True:
 		print(sign.SignIn(kw,myInfo["BDUSS"]))
 	if thisResult == 2:
 		print ("Signning...")
-		task.SignAll(myInfo["BDUSS"],myInfo["STOKEN"])
+		Task.SignAll()
+	if thisResult == 3:
+		when = {}
+		print ("When you want me to sign in everyday?/n")
+		when["hour"] = int(input("The hour is:(0-23)"))
+		while when["hour"]<0 or when["hour"]>23 :
+			print("given value out of the range")
+			when["hour"] = int(input("The hour is:(0-23)")) 
+		
+		when["minutes"] = int(input("The minutes is:(0-59)"))
+		while when["minutes"]<0 or when["minutes"]>59 :
+			print ("given value out of the range")
+			when["minutes"] = int(input("The minutes is:(0-59)"))	
+		Thread = task.AutoSignInThread(when,myInfo)
+		Thread.start()
+	if thisResult == 4 :
+		print("stoped")#Task.SetAutoSignInStop()

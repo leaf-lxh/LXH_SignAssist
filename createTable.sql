e2fd3a76b3742df8f1dd1dce459b4635506c3fd7
leaf-lxh <meleaf@qq.com>
2018-02-24 23:59:09 +0800

    A script for creating a MySQL database and a table.

---
+++ createTable.sql
@@ -0,0 +1,13 @@
+create database LXH_SignAssist character set gbk;
+
+use LXH_SignAssist;
+
+create table baidu_user_info
+(
+	id int unsigned not null auto_increment primary key,
+	BDUSS TEXT(300) not null,
+	TIEBA_STOKEN TEXT(200) not null,
+	TIEBA_USERNAME char(20) not null,
+	DAY_LASTSIGNIN int not null,
+	E_MAIL TEXT(200) 
+);

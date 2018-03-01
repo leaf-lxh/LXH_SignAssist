dfca3b5492d96dc72512c396cfa3782177483329
leaf-lxh <meleaf@qq.com>
2018-03-01 16:26:21 +0800

    added addUserInfo()

--- MySQLQuery.py
+++ MySQLQuery.py
@@ -1,3 +1,5 @@
+#!python3
+# coding:utf-8
 #MySQLQuery.py
 import pymysql
 
@@ -12,7 +14,7 @@ class MySQLOperate:
 		database = pymysql.connect(self.hostname, self.username, self.password, self.databaseName)
 		return database
 	
-	def QueryPrimarykey(self, tieba_username)
+	def QueryPrimarykey(self, tieba_username):
 		database = self.Connect()
 		
 		cursor = database.cursor()
@@ -23,16 +25,16 @@ class MySQLOperate:
 			result = cursor.fetchone()
 			id = result[0]
 		except:
-			id = -1
+			id = -1 #if there is no result,return -1
 		database.close()	
 		return id
 	
-	def QueryUserinfo(self,id):
+	def QueryUserInfo(self,id):
 		database = self.Connect()
 		
 		cursor = database.cursor()
 		
-		statement = 'SELECT * FROM %s WHERE id = %d' %(self.tablename, id)
+		statement = 'SELECT * FROM %s WHERE id = %d' %(self.tablename, int(id))
 		UserInfo = {}
 		try:
 			cursor.excute(statement)
@@ -43,5 +45,20 @@ class MySQLOperate:
 			UserInfo["BDUSS"] = result[1]
 			UserInfo["STOKEN"] = result[2]
 		except:
-			UserInfo = None
+			UserInfo = None #if there is no result, return None
+		database.close()
 		return UserInfo
+
+	def AddUserInfo(self,BDUSS,STOKEN,USERNAME,E_MAIL='NULL'):
+		database = self.Connect()
+
+		cursor = database.cursor()
+	
+		statment = """INSERT INTO %s (BDUSS,TIEBA_STOKEN,TIEBA_USERNAME,DAY_LASTSIGNIN,E_MAIL)
+				VALUES(%s,%s,%s,0,%s)""" %(self.tablename,BDUSS,STOKEN,USERNAME,E_MAIL)
+		try:
+			cursor.excute(statement)
+			db.commit()
+		except:
+			db.rollback()
+		return
